0d1c1ee8bd8f930ca35ba4e974253d4408f6c2dc
Tirawat Pompakunsawat <bamiowen@gmail.com>
2025-05-04 22:59:40 +1000

    add auth to drive route

--- src/routes/drive.route.tsx
+++ src/routes/drive.route.tsx
@@ -1,3 +1,4 @@
+import { SignIn, useAuth } from "@clerk/tanstack-react-start";
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/drive")({
@@ -5,6 +6,14 @@ export const Route = createFileRoute("/drive")({
 });
 
 function DriveComponent() {
+  const user = useAuth();
+  if (!user.userId) {
+    return (
+      <div className="flex items-center justify-center p-12">
+        <SignIn routing="hash" forceRedirectUrl={window.location.href} />
+      </div>
+    );
+  }
   return (
     <div>
       <Outlet />
